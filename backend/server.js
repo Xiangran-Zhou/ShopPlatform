d677355f4f74aadf94621da42cf9d9ad788a2c5d
kevinzhou <kevinzhou716@gmail.com>
2024-08-06 23:52:35 -0700

    fix connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import dotenv from "dotenv";
+dotenv.config();
 connectDB();
 const app = express();
-const port = 8001;
+const port = process.env.PORT || 8001;
 
-app.use(cors());
+//app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
